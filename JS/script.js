42283197f3e326821960cf2c55b09a5ca96ee3cc
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-19 20:24:12 +0300

    Use jQuery to create toggle effect for icons on the 'What We Do' section

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 $(".bg-image").hover(function() {
   $(".bg-image").addClass("top-section");
 });
+
+$("#designImage").click(function() {
+  $("#design").toggle();
+  $("#designImage").toggle();
+});
+
+$("#devImage").click(function() {
+  $("#development").toggle();
+  $("#devImage").toggle();
+});
+
+$("#productImage").click(function() {
+  $("#product").toggle();
+  $("#productImage").toggle();
+});
+
 });
