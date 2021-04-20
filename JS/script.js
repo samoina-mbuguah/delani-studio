866e73cf6347cc34de980d247520f95ab6a1368f
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-20 21:18:15 +0300

    Create hover effect for portfolio images using jQuery and CSS

--- JS/script.js
+++ JS/script.js
@@ -15,17 +15,108 @@ $("#productImage").click(function() {
   $("#product").toggle();
 });
 
-$(".text").hide();
-$(".image").animate({
+$("#blackie-text").hide();
+$("#blackie").animate({
   opacity:1
 });
 
-$(".image").hover(function() {
+$("#blackie").hover(function() {
   $(this).stop().animate({opacity:0.4},200);
-  $(".text").fadeIn();
+  $("#blackie-text").fadeIn();
 }, function() {
   $(this).stop().animate({opacity:1},500)
-  $(".text").fadeOut();
+  $("#blackie-text").fadeOut();
+});
+
+$("#ontario-text").hide();
+$("#ontario").animate({
+  opacity:1
+});
+
+$("#ontario").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#ontario-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#ontario-text").fadeOut();
+});
+
+$("#orange-text").hide();
+$("#orange").animate({
+  opacity:1
+});
+
+$("#orange").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#orange-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#orange-text").fadeOut();
+});
+
+$("#jim-carrey-text").hide();
+$("#jim-carrey").animate({
+  opacity:1
+});
+
+$("#jim-carrey").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#jim-carrey-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#jim-carrey-text").fadeOut();
+});
+
+$("#elena-text").hide();
+$("#elena").animate({
+  opacity:1
+});
+
+$("#elena").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#elena-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#elena-text").fadeOut();
+});
+
+$("#wordpress-text").hide();
+$("#wordpress").animate({
+  opacity:1
+});
+
+$("#wordpress").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#wordpress-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#wordpress-text").fadeOut();
+});
+
+$("#burned-logo-text").hide();
+$("#burned-logo").animate({
+  opacity:1
+});
+
+$("#burned-logo").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#burned-logo-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#burned-logo-text").fadeOut();
+});
+
+$("#giraffe-text").hide();
+$("#giraffe").animate({
+  opacity:1
+});
+
+$("#giraffe").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $("#giraffe-text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $("#giraffe-text").fadeOut();
 });
 
 // Tried using the array method and forEach so that every time someone hovers over an image it chooses the text from the second array and displays it, but I can't seem to wrap my mind around it :(
