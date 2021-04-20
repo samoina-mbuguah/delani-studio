8f89a40b48120d03b41b2d69842da6134d9b53bc
samoina mbuguah <samoina.mbuguah@student.moringaschool.com>
2021-04-20 15:35:21 +0300

    Use jQuery to add hover function to display project names in Portfolio section

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,46 @@ $(".bg-image").hover(function() {
 
 $("#designImage").click(function() {
   $("#design").toggle();
-  $("#designImage").toggle();
-});
+}); 
 
 $("#devImage").click(function() {
   $("#development").toggle();
-  $("#devImage").toggle();
 });
 
 $("#productImage").click(function() {
   $("#product").toggle();
-  $("#productImage").toggle();
 });
+
+$(".text").hide();
+$(".image").animate({
+  opacity:1
+});
+
+$(".image").hover(function() {
+  $(this).stop().animate({opacity:0.4},200);
+  $(".text").fadeIn();
+}, function() {
+  $(this).stop().animate({opacity:1},500)
+  $(".text").fadeOut();
+});
+
+// Tried using the array method and forEach so that every time someone hovers over an image it chooses the text from the second array and displays it, but I can't seem to wrap my mind around it :(
+
+//This was an array for images i have given ids, and next array texts to be displayed.
+
+//  let portfolioImages= ["#blackie", "#mightyOntario", "#orangePark", "#jimCarrey", "#elena", "#wordpress", "#burnedLogo", "#giraffe"];
+  
+//   let texts=["Blackie Project", "Mighty Ontario Project", "Orange Park Project", "Jim Carrey Project", "Elena Project", "Wordpress Project", "Burned Logo Project", "Giraffe Project"];
+  
+//   portfolioImages.forEach(function(portfolioImage) {
+//     $("portfolioImage").hover(function() {
+//       $("portfolioImage").stop().animate({opacity:0.4},200);
+//       $("texts").fadeIn();
+//     }, function() {
+//       $("portfolioImage").stop().animate({opacity:1},500)
+//       $("texts").fadeOut();
+//   });
+  
+//   });
 
 });
